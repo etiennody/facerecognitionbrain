639171d9cee15f70174f5d8428453ea1e3d01ffd
etiennody <etiennody@gmail.com>
2021-03-04 23:49:58 +0100

    added Particles component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -7,6 +8,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 800
+              }
+            }
+          }
+        }}
+      />
       <Navigation />
       <Logo />
       <Rank />
