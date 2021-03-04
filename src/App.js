e1181175e9c107cd80d6a0e25f7da5462a059770
etiennody <etiennody@gmail.com>
2021-03-04 21:46:46 +0100

    added Logo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      {/* 
       <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
