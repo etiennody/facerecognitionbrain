c182e2e913f100aa1600ca74769b99f7f090bbdf
etiennody <etiennody@gmail.com>
2021-03-05 16:55:35 +0100

    added image style

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className='center'>
-      <img
-        alt=''
-        src={imageUrl}
-      />
+    <div className='center ma'>
+      <div className='absolute mt3'>
+        <img
+          alt=''
+          src={imageUrl}
+          width='500px'
+          height='auto'
+        />
+      </div>
     </div>
   );
 }
