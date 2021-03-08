33efa6d3289bcfbe5e8c5f00431900fbbc090fa5
etiennody <etiennody@gmail.com>
2021-03-08 22:48:38 +0100

    added name and entries into Rank component

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({ username, entries }) => {
     return (
         <div>
             <div className='white f3'>
-                {'Jody, your current rank is...'}
+                {`${username}, your current rank is...`}
             </div>
             <div className='white f1'>
-                {'#5'}
+                {entries}
             </div>
         </div>
     );
